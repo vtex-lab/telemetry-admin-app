f64d4c29af1e1d9ad5030ee56bf9bbf4f9d4cdb9
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2023-09-19 11:24:17 -0300

    first commit

---
+++ react/telemetry.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+const Telemetry: FC = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>I&apos;ll be a telemetry component</p>
+    </>
+  )
+}
+
+export default Telemetry
